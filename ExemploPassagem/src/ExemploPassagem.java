e8484d2caebdf9d004c3e6826e3062c58564d3fc
AndroidAbreu <bolsa.abreu@yahoo.com>
2016-10-02 00:04:55 +0100

    Exemplos simples

---
+++ ExemploPassagem/src/ExemploPassagem.java
@@ -0,0 +1,34 @@
+
+public class ExemploPassagem {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.print("Vamos começar :");
+		
+		System.out.println("Primeira linha");
+		
+		espaco();
+		
+		System.out.println("Segunda linha");
+		
+		espacotrees();
+		
+		System.out.println("Terceira linha");
+	}
+
+	private static void espacotrees() {
+		// TODO Auto-generated method stub
+		espaco(); 
+		espaco();
+		espaco();
+	}
+
+	private static void espaco() {
+		// TODO Auto-generated method stub
+		
+		System.out.println("");
+		
+	}
+
+}
