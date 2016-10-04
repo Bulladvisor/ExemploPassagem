d2243c400790f13d7ca629443b730fbd87e53fbf
AndroidAbreu <bolsa.abreu@yahoo.com>
2016-10-04 12:33:30 +0100

    Termina Estudo String fase 2

---
+++ ExemploPassagem/src/AnexoTres.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class AnexoTres {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		        Scanner scan = new Scanner(System.in);
+		        
+		        int i = scan.nextInt();
+		        
+		        //Colocar o valor com virgula, senão dá erro no double
+		        
+		        double d = scan.nextDouble();
+		        
+                String s = new StringBuilder(scan.next()).toString();
+                
+                
+                
+		        
+		        
+		        
+		
+		        //Atenção que não aparece a string toda 
+		        
+                
+
+		      
+		        System.out.println("String: " + s);
+		        System.out.println("Double: " + d);
+		        System.out.println("Int: " + i);
+	}
+
+}
--- ExemploPassagem/src/AnexoUm.java
+++ ExemploPassagem/src/AnexoUm.java
@@ -1,9 +1,32 @@
+import java.util.Scanner;
 
 public class AnexoUm {
+	
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		String myString = scanner.next();
+		int myInt = scanner.nextInt();
+		
+		System.out.println("Coloca valor a : ");
+		
+		int a = scanner.nextInt();
+		
+		System.out.println("Coloca valor b : ");
+		
+		int b = scanner.nextInt();
+		scanner.close();
 
+		
+		
+		System.out.println("myString is: " + myString);
+		System.out.println("myInt is: " + myInt);
+		System.out.println(a);
+		System.out.println(b);
 	}
 
 }
